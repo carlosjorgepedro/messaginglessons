ec649cac9ea3c00be11ff4e19e587f8111ef73a8
Carlos Pedro <Carlos.Pedro@immform.org.uk>
2018-02-15 13:36:29 +0000

    messaging-with-rabbitmq-and-net-review-part-4

---
+++ RabbitMq.OneWayMessage.Receiver/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMq.OneWayMessage.Receiver
+{
+    class Program
+    {
+        static void Main()
+        {
+            ReceiveSingleOneWayMessage();
+        }
+
+        static void ReceiveSingleOneWayMessage()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                VirtualHost = "accounting",
+                UserName = "accountant",
+                Password = "accountant"
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.BasicQos(0, 1, false);
+            var basicConsumer = new OneWayMessageReceiver(channel);
+            channel.BasicConsume("my.first.queue", false, basicConsumer);
+        }
+    }
+}
