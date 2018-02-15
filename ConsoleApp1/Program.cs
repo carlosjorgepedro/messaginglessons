9b41730473ade72d4b28fa7184005a188a60f44e
Carlos Pedro <Carlos.Pedro@immform.org.uk>
2018-02-15 14:48:33 +0000

    Messaging with RabbitMQ and .NET review part 6: the fanout exchange type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,45 @@ namespace ConsoleApp1
     {
         static void Main()
         {
+            var connection = GetConnection();
+            Console.WriteLine($"Connection open: {connection.IsOpen }");
+
+            var channel = connection.CreateModel();
+            Console.WriteLine($"Channel open: {channel.IsOpen }");
+
+            SetupFanoutExchange(connection, channel);
+
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+            Console.WriteLine("Main done...");
+            Console.ReadKey();
+        }
+
+
+        static void SetupFanoutExchange(IConnection connection, IModel channel)
+        {
+
+            //declare the common exchange
+            channel.ExchangeDeclare("pt.southbank.fanout.exchange", ExchangeType.Fanout, true, false, null);
+            //declare queues
+            channel.QueueDeclare("pt.southbank.queues.accounting", true, false, false, null);
+            channel.QueueDeclare("pt.southbank.queues.management", true, false, false, null);
+            //bind queues to exchange
+            channel.QueueBind("pt.southbank.queues.accounting","pt.southbank.fanout.exchange", string.Empty, null);
+            channel.QueueBind("pt.southbank.queues.management","pt.southbank.fanout.exchange", string.Empty, null);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            properties.ContentType = "text/plain";
+            var address = new PublicationAddress(ExchangeType.Fanout, "pt.southbank.fanout.exchange", "null");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("A new huge order has just come in worth $1M!!!!!"));
+
+            channel.Close();
+            connection.Close();
+        }
+
+
+        static IConnection GetConnection()
+        {
             var connectionFactory = new ConnectionFactory
             {
                 HostName = "localhost",
@@ -19,13 +58,11 @@ namespace ConsoleApp1
             };
 
             var connection = connectionFactory.CreateConnection();
-            Console.WriteLine($"Connection open: {connection.IsOpen }");
+            return connection;
+        }
 
-
-            var channel = connection.CreateModel();
-            Console.WriteLine($"Channel open: {channel.IsOpen }");
-
-
+        static void SetUpDirectExchange(IConnection connection, IModel channel)
+        {
             //Create exchange
             channel.ExchangeDeclare("my.first.exchange", ExchangeType.Direct, true, false, null);
             //Create Queue where messsages should be stored
@@ -38,15 +75,12 @@ namespace ConsoleApp1
             properties.Persistent = true;
             properties.ContentType = "text/plain";
             var address = new PublicationAddress(ExchangeType.Direct, "my.first.exchange", string.Empty);
-            channel.BasicPublish(address,  properties, Encoding.UTF8.GetBytes("this is my first message."));
-            
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("this is my first message."));
+
 
             channel.Close();
             connection.Close();
-
-            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
-            Console.WriteLine("Main done...");
-            Console.ReadKey();
         }
     }
 }
+
---
+++ RabbitMq.Fanout.Receiver.Accounting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMq.Fanout.Receiver.Accounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReceiveFanoutMessages();
+        }
+
+        static void ReceiveFanoutMessages()
+        {
+            var connection = GetConnection();
+            var channel = connection.CreateModel();
+            channel.BasicQos(0, 1, false);
+            var eventingConsumer = new EventingBasicConsumer(channel);
+            eventingConsumer.Received += (sender, args) =>
+            {
+                var basicProperties = args.BasicProperties;
+
+                Debug.WriteLine(string.Concat("Message received from the exchange ", args.Exchange));
+                Debug.WriteLine(string.Concat("Content type: ", basicProperties.ContentType));
+                Debug.WriteLine(string.Concat("Consumer tag: ", args.ConsumerTag));
+                Debug.WriteLine(string.Concat("Delivery tag: ", args.DeliveryTag));
+                var message = Encoding.UTF8.GetString(args.Body);
+                Debug.WriteLine(string.Concat("Message: ", Encoding.UTF8.GetString(args.Body)));
+                Console.WriteLine(string.Concat("Message received by the accounting consumer: ", message));
+                channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            channel.BasicConsume("pt.southbank.queues.accounting", false, eventingConsumer);
+        }
+
+
+        static IConnection GetConnection()
+        {
+            return new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                VirtualHost = "accounting",
+                UserName = "accountant",
+                Password = "accountant"
+            }.CreateConnection();
+        }
+
+    }
+}
