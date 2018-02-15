77cc976174465ef3bab3b5387b6b768bb24b9997
Carlos Pedro <Carlos.Pedro@immform.org.uk>
2018-02-15 14:02:56 +0000

    Messaging with RabbitMQ and .NET review part 5: one way messaging with an event based consumer

--- RabbitMq.OneWayMessage.Receiver/Program.cs
+++ RabbitMq.OneWayMessage.Receiver/Program.cs
@@ -1,17 +1,24 @@
-﻿using RabbitMQ.Client;
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMq.OneWayMessage.Receiver
 {
     class Program
     {
+        static IModel _channelForEventing;
+
         static void Main()
         {
-            ReceiveSingleOneWayMessage();
+            ReceiveMessageWithEvents();
+            //            ReceiveSingleOneWayMessage();
         }
 
-        static void ReceiveSingleOneWayMessage()
+        static ConnectionFactory GetConnectionFactory()
         {
-            var connectionFactory = new ConnectionFactory
+            return new ConnectionFactory
             {
                 HostName = "localhost",
                 Port = 5672,
@@ -19,12 +26,36 @@ namespace RabbitMq.OneWayMessage.Receiver
                 UserName = "accountant",
                 Password = "accountant"
             };
+        }
 
-            var connection = connectionFactory.CreateConnection();
+
+        static void ReceiveSingleOneWayMessage()
+        {
+            var connection = GetConnectionFactory().CreateConnection();
             var channel = connection.CreateModel();
             channel.BasicQos(0, 1, false);
             var basicConsumer = new OneWayMessageReceiver(channel);
             channel.BasicConsume("my.first.queue", false, basicConsumer);
+        }
+
+        static void ReceiveMessageWithEvents()
+        {
+            var connection = GetConnectionFactory().CreateConnection();
+            _channelForEventing = connection.CreateModel();
+            _channelForEventing.BasicQos(0, 1, false);
+            var eventingConsumer = new EventingBasicConsumer(_channelForEventing);
+            eventingConsumer.Received += (sender, args) =>
+            {
+                var basicProperties = args.BasicProperties;
+                Console.WriteLine("Message received by the event based consumer. Check the debug window for details.");
+                Debug.WriteLine(string.Concat("Message received from the exchange ", args.Exchange));
+                Debug.WriteLine(string.Concat("Content type: ", basicProperties.ContentType));
+                Debug.WriteLine(string.Concat("Consumer tag: ", args.ConsumerTag));
+                Debug.WriteLine(string.Concat("Delivery tag: ", args.DeliveryTag));
+                Debug.WriteLine(string.Concat("Message: ", Encoding.UTF8.GetString(args.Body)));
+                _channelForEventing.BasicAck(args.DeliveryTag, false);
+            };
+            _channelForEventing.BasicConsume("my.first.queue", false, eventingConsumer);
         }
     }
 }
